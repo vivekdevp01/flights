aec3d52a908761d87b1e6680efeaaa20298b522c
Vivek Singh Pundir <23f100036@ds.study.iitm.ac.in>
2024-04-11 16:38:04 +0530

    Added one to many sequelize method where we have done create get and delete operation using foreign key

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT,async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    // bad code
+    const {Airport,City}=require('./models');
+    const city=await City.findByPk(1);
+console.log(city);
+const response=await city.createAirport({name:"keepa", code:'BLR'});
+const getAirport=await city.getAirports();
+console.log(getAirport);
+await City.destroy({
+    where:{
+        id:1
+    }
 });
+
+});
+
+
