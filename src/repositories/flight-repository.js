ccb06b8045cabd786aa99ba07ea43abe935429d2
Vivek Singh Pundir <23f100036@ds.study.iitm.ac.in>
2024-04-13 19:52:47 +0530

    done on and as function use

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,12 +1,46 @@
 const CrudRepository=require('./crud-repository');
-const { Flight }=require('../models');
+const Sequelize=require('sequelize')
+const { Flight, Airplane, Airport, City }=require('../models');
 class FlightRepository extends CrudRepository{
     constructor(){
         super(Flight);
     }
-    async getAllFlights(filter){
+    async getAllFlights(filter,sort){
         const response=await Flight.findAll({
-            where:filter
+            where:filter,
+            order:sort,
+           include:[ {
+            model: Airplane,
+            required: true,
+            as:'airplane_detail',
+            
+           },
+           {
+            model: Airport,
+            required:true,
+            as:'departure_airport',
+            on:{
+                col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departure_airport.code") )
+            }
+            ,
+            include:{
+                model:City,
+                required:true
+            }
+           },
+           {
+            model: Airport,
+            required:true,
+            as:'arrival_airport',
+            on:{
+                col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrival_airport.code") )
+            },
+            include:{
+                model:City,
+                required:true
+            }
+           }
+        ]
         });
         return response;
 
